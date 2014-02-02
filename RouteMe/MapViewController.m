dcec0ad9152e51783c523ff0e3e311f19a25aea7
Jason Humphries <humphriesj@gmail.com>
2014-02-02 11:51:52 -0500

    implementing the path based searching

--- RouteMe/MapViewController.m
+++ RouteMe/MapViewController.m
@@ -136,29 +136,96 @@
 -(void)addDirections:(NSDictionary *)json
 {
     if ([json[@"routes"] count] > 0) {
+//        NSDictionary *firstRoute = json[@"routes"][0];
+//        NSArray *firstRouteLegs = firstRoute[@"legs"];
+//        float routeStartLat = [firstRoute[@"legs"][0][@"start_location"][@"lat"] floatValue];
+//        float routeStartLng = [firstRoute[@"legs"][0][@"start_location"][@"lng"] floatValue];
+//        float routeEndLat = [firstRoute[@"legs"][0][@"end_location"][@"lat"] floatValue];
+//        float routeEndLng = [firstRoute[@"legs"][0][@"end_location"][@"lng"] floatValue];
+        
+//        float x1 = routeStartLat;
+//        float y1 = routeStartLng;
+//        float x2 = routeEndLat;
+//        float y2 = routeEndLng;
+        
+//        NSArray *steps = firstRouteLegs[0][@"steps"];
+//        float totalDistanceValue = [firstRouteLegs[0][@"distance"][@"value"] floatValue]; // meters
+//        float totalDistanceTime = [firstRouteLegs[0][@"duration"][@"value"] floatValue]; // seconds
+//        float searchRadius = 2000.f; // interval on the line to search
+//        float searchCircleNumber = ceilf(totalDistanceValue/(2.0*searchRadius));
+//        for (float j=0; j<searchCircleNumber; j++) {
+//            float currentLaty = x1 + ((x2 - x1)/(j+1))*j;
+//            float currentLngy = y1 + ((y2 - y1)/(j+1))*j;
+//            CLLocationCoordinate2D currentCoord = CLLocationCoordinate2DMake(currentLaty, currentLngy);
+//            NSString *searchTerm = MY_APP_DELEGATE.keywordSearchString.length > 0 ? MY_APP_DELEGATE.keywordSearchString : @"coffee";
+//            NSLog(@"about to search (%@) lat(%f) lng(%f)", searchTerm, currentCoord.latitude, currentCoord.longitude);
+//            [GooglePlacesSearcher searchKeyword:searchTerm
+//                                          atLat:currentCoord.latitude
+//                                            lng:currentCoord.longitude
+//                                        success:
+//             ^(NSArray *results) {
+//                 dispatch_async(dispatch_get_main_queue(), ^{
+//                     [self reloadPlacesArrayLocations:results];
+//                 });
+//             } failure:^(NSError *error) {
+//                 NSLog(@"error0: %@", error.localizedDescription);
+//             }];
+//        }
+        
+//        float totalDistance = 0.f;
+//        float totalTime = 0.f;
+//        int i = 0;
+//        for (NSDictionary *leg in steps) {
+//            NSLog(@"distance:%@", leg[@"distance"][@"text"]);
+//            float distanceValue = [leg[@"distance"][@"value"] floatValue]; // meters
+//            float distanceTime = [leg[@"duration"][@"value"] floatValue]; // seconds
+//            totalDistance += distanceValue;
+//            totalTime += distanceTime;
+//            NSLog(@"time:%@", leg[@"duration"][@"text"]);
+//            NSLog(@"instructions:%@", leg[@"html_instructions"]);
+//            NSLog(@"-------step %d--------", i+1);
+//        }
+//        
+//        NSLog(@"firstRoute Leg Steps(%lu)", (unsigned long)steps.count);
+//        NSLog(@"totalDistance: %f", totalDistance);
+//        NSLog(@"easy total distance: %@", firstRouteLegs[0][@"distance"][@"value"]);
+//        NSLog(@"totalTime: %f", totalTime);
+//        NSLog(@"easy total time: %@", firstRouteLegs[0][@"duration"][@"value"]);
+        
+//        for (int i = 0; i < self.mainPath.count; i++) {
+//            CLLocationCoordinate2D thisCoord = [self.mainPath coordinateAtIndex:i];
+//            NSLog(@"coord:[%f,%f]", thisCoord.latitude, thisCoord.longitude);
+//        }
+        
         NSDictionary *firstRoute = json[@"routes"][0];
         NSArray *firstRouteLegs = firstRoute[@"legs"];
-        float routeStartLat = [firstRoute[@"legs"][0][@"start_location"][@"lat"] floatValue];
-        float routeStartLng = [firstRoute[@"legs"][0][@"start_location"][@"lng"] floatValue];
-        float routeEndLat = [firstRoute[@"legs"][0][@"end_location"][@"lat"] floatValue];
-        float routeEndLng = [firstRoute[@"legs"][0][@"end_location"][@"lng"] floatValue];
+        float totalDistanceValue = [firstRouteLegs[0][@"distance"][@"value"] floatValue];
         
-        float x1 = routeStartLat;
-        float y1 = routeStartLng;
-        float x2 = routeEndLat;
-        float y2 = routeEndLng;
+        NSDictionary *routeOverviewPolyline = firstRoute[@"overview_polyline"];
+        NSString *overview_route = routeOverviewPolyline[@"points"];
+        self.mainPath = [GMSPath pathFromEncodedPath:overview_route];
+        int numberOfPoints = self.mainPath.count;
+        int numberOfSearches = (int)floor((numberOfPoints/10.0));
+        // total number of points along path = self.mainPath.count
+        NSLog(@"totalDistance: %f", totalDistanceValue);
+        NSLog(@"numberOfPoints: %d", numberOfPoints);
+        NSLog(@"numberOfSearches: %d", numberOfSearches);
         
-        NSArray *steps = firstRouteLegs[0][@"steps"];
-        float totalDistanceValue = [firstRouteLegs[0][@"distance"][@"value"] floatValue]; // meters
-        float totalDistanceTime = [firstRouteLegs[0][@"duration"][@"value"] floatValue]; // seconds
-        float searchRadius = 2000.f; // interval on the line to search
-        float searchCircleNumber = ceilf(totalDistanceValue/(2.0*searchRadius));
-        for (float j=0; j<searchCircleNumber; j++) {
-            float currentLaty = x1 + ((x2 - x1)/(j+1))*j;
-            float currentLngy = y1 + ((y2 - y1)/(j+1))*j;
-            CLLocationCoordinate2D currentCoord = CLLocationCoordinate2DMake(currentLaty, currentLngy);
+        for (int j=0; j < numberOfSearches; j++) {
+            CLLocationCoordinate2D currentCoord = [self.mainPath coordinateAtIndex:(j*numberOfSearches)];
+            CLLocation *currentLoc = [[CLLocation alloc] initWithLatitude:currentCoord.latitude longitude:currentCoord.longitude];
+            
+            CLLocationCoordinate2D lastCoord;
+            CLLocation *lastLoc;
+            CGFloat distanceFromLastLocation = 0;
+            if (j != 0) {
+                lastCoord = [self.mainPath coordinateAtIndex:((j-1)*numberOfSearches)];
+                lastLoc = [[CLLocation alloc] initWithLatitude:lastCoord.latitude longitude:lastCoord.longitude];
+                distanceFromLastLocation = [lastLoc distanceFromLocation:currentLoc];
+                NSLog(@"distanceFromLastLocation: %f", distanceFromLastLocation);
+            }
             NSString *searchTerm = MY_APP_DELEGATE.keywordSearchString.length > 0 ? MY_APP_DELEGATE.keywordSearchString : @"coffee";
-            NSLog(@"about to search (%@) lat(%f) lng(%f)", searchTerm, currentCoord.latitude, currentCoord.longitude);
+            NSLog(@"%f,%f", currentCoord.latitude, currentCoord.longitude);
             [GooglePlacesSearcher searchKeyword:searchTerm
                                           atLat:currentCoord.latitude
                                             lng:currentCoord.longitude
@@ -170,37 +237,8 @@
              } failure:^(NSError *error) {
                  NSLog(@"error0: %@", error.localizedDescription);
              }];
-            
         }
         
-        float totalDistance = 0.f;
-        float totalTime = 0.f;
-        int i = 0;
-        for (NSDictionary *leg in steps) {
-            NSLog(@"distance:%@", leg[@"distance"][@"text"]);
-            float distanceValue = [leg[@"distance"][@"value"] floatValue]; // meters
-            float distanceTime = [leg[@"duration"][@"value"] floatValue]; // seconds
-            totalDistance += distanceValue;
-            totalTime += distanceTime;
-            NSLog(@"time:%@", leg[@"duration"][@"text"]);
-            NSLog(@"instructions:%@", leg[@"html_instructions"]);
-            NSLog(@"-------step %d--------", i+1);
-        }
-        
-        NSLog(@"firstRoute Leg Steps(%lu)", (unsigned long)steps.count);
-        NSLog(@"totalDistance: %f", totalDistance);
-        NSLog(@"easy total distance: %@", firstRouteLegs[0][@"distance"][@"value"]);
-        NSLog(@"totalTime: %f", totalTime);
-        NSLog(@"easy total time: %@", firstRouteLegs[0][@"duration"][@"value"]);
-        
-        NSDictionary *routeOverviewPolyline = firstRoute[@"overview_polyline"];
-        NSString *overview_route = routeOverviewPolyline[@"points"];
-        self.mainPath = [GMSPath pathFromEncodedPath:overview_route];
-        // total number of points along path = self.mainPath.count
-        for (int i = 0; i < self.mainPath.count; i++) {
-            CLLocationCoordinate2D thisCoord = [self.mainPath coordinateAtIndex:i];
-            NSLog(@"coord:[%f,%f]", thisCoord.latitude, thisCoord.longitude);
-        }
         GMSPolyline *polyline = [GMSPolyline polylineWithPath:self.mainPath];
         polyline.strokeWidth = 8.f;
         polyline.strokeColor = [UIColor blueColor];
